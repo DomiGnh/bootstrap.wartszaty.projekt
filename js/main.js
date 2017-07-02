fa72198ceeac4e9efafb68f5a973763d8d2a49aa
Dominika Gnich <d.gnich@wp.pl>
2017-07-02 14:14:09 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,36 @@
-'use strict';
-$(function(){
+/*'use strict';
+
+
+$(function () {
+   $(window).scroll(function () {
+      if ($(window).scrollTop() >= 50) {
+          $('#main-nav').addClass('scroll');
+          $('#main-nav').css({'transition': 'all 0.3s'});
+      } else {
+           $('#main-nav').removeClass('scroll');
+      }
     
-    $('.carousel').carousel({
-        interval: 2000
-    });
+ });
+    
+    if ($(window).scrollTop() >= 50){
+        $('#main-nav').addClass('scroll');
+    }
+});*/
+
+
+'use strict';
+
+
+$(function () {
+	$(window).scroll(function () {
+		if ($(window).scrollTop() >= 50) {
+			$("#main-nav").addClass("scroll");
+			$("#main-nav").css({"transition":"all 0.3s"});
+		} else {
+			$("#main-nav").removeClass("scroll");
+		}
+	});
+	if ($(window).scrollTop() >= 50) {
+			$("#main-nav").addClass("scroll");
+	}
 });
-
-
